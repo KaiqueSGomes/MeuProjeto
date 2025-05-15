ce135f32ddf76ff25206e3717e5e382c1ee9ab9a
KaiqueSGomes <kaiquesgmes@gmail.com>
2025-05-14 23:46:45 -0300

    atualizando db.js

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Ka980548244@',
-  database: 'dbQuizPex'
-});
+const db = process.env.DATABASE_URL
+  ? mysql.createPool(process.env.DATABASE_URL)  // se tiver a variável de ambiente (Railway)
+  : mysql.createPool({                          // se não tiver, usa local
+      host: 'localhost',
+      user: 'root',
+      password: 'Ka980548244@',
+      database: 'dbQuizPex'
+    });
 
 module.exports = db;
