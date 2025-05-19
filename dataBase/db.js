a28456dd01c88c49a2b8e92c5e256d4ae9175a8e
KaiqueSGomes <kaiquesgmes@gmail.com>
2025-05-18 23:11:46 -0300

    Passando dados sensíveis (dados de login do banco Railway) para .env;

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'metro.proxy.rlwy.net',
-  user: 'root',
-  password: 'BpVAlDabYpOzJLtEbKTWPAXuVBbUlIVR',
-  port: 14111,
-  database: 'railway',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
   }
